5aceb0bc72244d07921d891a245407e0fb35078f
AjiHardiNata <73481926+AjiHardiNata@users.noreply.github.com>
2021-04-18 05:06:27 +0700

    *to conf*

--- sample_config.py
+++ sample_config.py
@@ -1,7 +1,7 @@
-API_ID = 14371
-API_HASH = "e46b6c854d2bf58a0"
-SUDO_CHAT_ID = -1001485876964  # Chat where the bot will play the music.
-SUDOERS = [1243703097, 13216546] # Users which have special control over the bot.
+API_ID = 2763283
+API_HASH = "f1f6c384b963a9b0e0d784a05cd61f87"
+SUDO_CHAT_ID = -1001205521806  # Chat where the bot will play the music.
+SUDOERS = [1707278437,1669061195] # Users which have special control over the bot.
 
 # don't make changes below this line
 ARQ_API = "http://35.240.133.234:8000"
